a2eb5ba88996f67f2e9a1ac0b90ceab5da20a3f1
Tom de Visser <tom.d6r@outlook.com>
2024-02-10 14:52:24 +0100

    Added more styling to the body and mobile navigation

--- footer.php
+++ footer.php
@@ -1,39 +1,38 @@
 </main>
-	<footer id="site-footer">
-		<h2>
-			<span class="screen-reader-text">
-				<?php _e( 'Site footer', 'toms' ); ?>
-			</span>
-		</h2>
-		<div class="footer-columns">
+<footer id="site-footer">
+	<h2 class="screen-reader-text">
+		<?php _e('Site footer', 'toms'); ?>
+	</h2>
+	<div class="footer-columns">
+		<div class="container-large">
+			<div class="footer-columns-container widgets">
+				<div class="footer-column">
+					<?php dynamic_sidebar('footer-1'); ?>
+				</div>
+				<div class="footer-column">
+					<?php dynamic_sidebar('footer-2'); ?>
+				</div>
+				<div class="footer-column">
+					<?php dynamic_sidebar('footer-3'); ?>
+				</div>
+				<div class="footer-column">
+					<?php dynamic_sidebar('footer-4'); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<div class="copyright-wrapper">
+		<div class="copyright">
 			<div class="container-large">
-				<div class="footer-columns-container widgets">
-					<div class="footer-column">
-						<?php dynamic_sidebar( 'footer-1' ); ?>
-					</div>
-					<div class="footer-column">
-						<?php dynamic_sidebar( 'footer-2' ); ?>
-					</div>
-					<div class="footer-column">
-						<?php dynamic_sidebar( 'footer-3' ); ?>
-					</div>
-					<div class="footer-column">
-						<?php dynamic_sidebar( 'footer-4' ); ?>
-					</div>
-				</div>
+				<?php dynamic_sidebar('copyright'); ?>
 			</div>
 		</div>
+	</div>
+</footer>
 
-
-		<div class="copyright-wrapper">
-			<div class="copyright">
-				<div class="container-large">
-					<?php dynamic_sidebar( 'copyright' ); ?>
-				</div>
-			</div>
-		</div>
-	</footer>
-
-	<?php wp_footer(); ?>
+<?php wp_footer(); ?>
 </body>
+
 </html>
