d50f092c3575a56875e03a5d492fa8e723719e8e
Tom de Visser <tom.d6r@outlook.com>
2024-02-10 14:32:45 +0100

    Finished the footer and the columns block

--- layouts/columns/acf.php
+++ layouts/columns/acf.php
@@ -1,26 +1,54 @@
 <?php
 $layout_name = 'columns';
 
-$layouts[ $layout_name ] = array(
-	'order'      => 1,
-	'key'        => 'layout_' . $layout_name,
-	'name'       => $layout_name,
-	'label'      => __( 'Columns', 'toms' ),
-	'display'    => 'block',
-	'sub_fields' => array(
+$layouts[$layout_name] = array(
+	'order'        => 1,
+	'key'          => 'layout_' . $layout_name,
+	'name'         => $layout_name,
+	'label'        => __('Columns', 'toms'),
+	'display'      => 'block',
+	'button_label' => __('Add new column', 'toms'),
+	'sub_fields'   => array(
 		array(
 			'key'        => 'field_' . $layout_name . '_columns',
-			'label'      => __( 'Columns', 'toms' ),
+			'label'      => __('Columns', 'toms'),
 			'name'       => $layout_name . '_columns',
 			'type'       => 'repeater',
 			'required'   => 1,
+			'layout'     => 'block',
 			'sub_fields' => array(
 				array(
-					'key'      => 'field_' . $layout_name . '_column',
-					'label'    => __( 'Column', 'toms' ),
-					'name'     => $layout_name . '_column',
-					'type'     => 'wysiwyg',
+					'key'      => 'field_' . $layout_name . '_title',
+					'label'    => __('Title', 'toms'),
+					'name'     => $layout_name . '_title',
+					'type'     => 'text',
 					'required' => 1,
+				),
+				array(
+					'key'      => 'field_' . $layout_name . '_content',
+					'label'    => __('Content', 'toms'),
+					'name'     => $layout_name . '_content',
+					'type'     => 'textarea',
+					'rows'     => 3,
+					'required' => 1,
+				),
+				array(
+					'key'      => 'field_' . $layout_name . '_color',
+					'label'    => __('Color', 'toms'),
+					'name'     => $layout_name . '_color',
+					'type'     => 'radio',
+					'choices'  => array(
+						'yellow' => __('Yellow', 'toms'),
+						'blue'   => __('Blue', 'toms'),
+						'green' => __('Green', 'toms'),
+					),
+					'required' => 1,
+				),
+				array(
+					'key'      => 'field_' . $layout_name . '_link',
+					'label'    => __('Link', 'toms'),
+					'name'     => $layout_name . '_link',
+					'type'     => 'link',
 				),
 			),
 		),
--- layouts/columns/columns.php
+++ layouts/columns/columns.php
@@ -1,19 +1,41 @@
 <?php
-$layout_name = basename( __FILE__, '.php' );
+$layout_name = basename(__FILE__, '.php');
 $prefix      = $layout_name . '_';
-$columns     = get_sub_field( $prefix . 'columns' );
+$columns     = get_sub_field($prefix . 'columns');
 ?>
 <section class="columns">
 	<div class="container">
 		<div class="content">
 			<?php
-			if ( $columns ) {
-				foreach ( $columns as $column ) {
-					?>
-					<div class="column">
-						<?php echo $column['columns_column']; ?>
+			if ($columns) {
+				foreach ($columns as $column) {
+					$title = $column['columns_title'];
+					$content = $column['columns_content'];
+					$color = $column['columns_color'];
+					$link = $column['columns_link'];
+			?>
+					<div class="column <?php echo $color ? 'bg-light-' . $color : ''; ?>">
+						<?php
+						if ($title) {
+						?>
+							<h2 class="title"><?php echo $title; ?></h2>
+						<?php
+						}
+
+						if ($content) {
+						?>
+							<p><?php echo $content; ?></p>
+						<?php
+						}
+
+						if ($link['url']) {
+						?>
+							<a href="<?php echo $link['url']; ?>" class="overlay-link"></a>
+						<?php
+						}
+						?>
 					</div>
-					<?php
+			<?php
 				}
 			}
 			?>
