f35fcc0ec5e308c92551ad2b09303482dc1685d8
Tom de Visser <tom.d6r@outlook.com>
2024-02-10 19:07:35 +0100

    Added slider controls to the product slider

--- layouts/product-slider/product-slider.php
+++ layouts/product-slider/product-slider.php
@@ -52,7 +52,7 @@ if ('latest' === $show) {
 					}
 					?>
 				</div>
-				<div class="swiper-pagination"></div>
+				<!-- <div class="swiper-pagination"></div> -->
 				<div class="swiper-button-prev"></div>
 				<div class="swiper-button-next"></div>
 			</div>
--- source/javascript/product-slider.js
+++ source/javascript/product-slider.js
@@ -2,7 +2,7 @@ import Swiper from "swiper";
 import { Navigation, Pagination } from "swiper/modules";
 
 import "swiper/scss";
-// import "swiper/css/navigation";
+import "swiper/css/navigation";
 // import "swiper/css/pagination";
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -13,19 +13,22 @@ const initSliders = () => {
 	const swiper = new Swiper(".product-slider-wrapper", {
 		modules: [Navigation, Pagination],
 
-		slidesPerView: 2,
+		slidesPerView: 1,
 		spaceBetween: 16,
 
 		breakpoints: {
+			640: {
+				slidesPerView: 2,
+			},
 			1024: {
 				slidesPerView: 3,
 			},
 		},
 
 		// If we need pagination
-		pagination: {
-			el: ".swiper-pagination",
-		},
+		// pagination: {
+		// 	el: ".swiper-pagination",
+		// },
 
 		// Navigation arrows
 		navigation: {
